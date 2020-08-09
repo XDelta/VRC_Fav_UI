bb9e99f3fdc24cb5d5c56b9a8356d527eadf7754
Delta <XDelta@users.noreply.github.com>
2020-08-09 10:06:16 -0700

    Update install.bat

---
+++ debug.bat
@@ -0,0 +1,2 @@
+python VRC_Fav_UI.py 1> "debug.err.log" 2>&1
+pip freeze > "debug.pip.log"
--- install.bat
+++ install.bat
@@ -1,6 +1,9 @@
 @echo off
 cd /d %~dp0
+echo Installing requirements
 pip install -r requirements.txt
 echo.
-echo Make sure to edit config.json before running VRC_Fav_UI.py
+echo Make sure to edit config/config.toml before running VRC_Fav_UI.py
+echo If upgrading from a previous version, copy over your avatars and config folders
+echo Have an issue? Create an issue here https://github.com/XDelta/VRC_Fav_UI
 pause
